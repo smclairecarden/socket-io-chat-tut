3567d8bd304d1e2424787c9b4bf65193dcdae154
Claire Carden <smclairecarden@gmail.com>
2022-05-05 11:56:25 -0500

    add styling

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react'
-
+import ScrollToBottom from "react-scroll-to-bottom"
 
 function Chat({socket, userName, room}) {
   const [currentMessage, setCurrentMessage] = useState("")
+  const [messageList, setMessageList] = useState([])
 
   const sendMessage = async () => {
     if(currentMessage !== "") {
@@ -13,27 +14,41 @@ function Chat({socket, userName, room}) {
         time: new Date(Date.now()),
       }
       await socket.emit("send_message", messageData)
-
+      setMessageList((list) => [...list, messageData])
     }
   }
 
 
   useEffect(() => {
     socket.on("receive_message", (data) => {
-        console.log(data)
+        setMessageList((list) => [...list, data])
     })
   }, [socket]) 
 
   return (
-    <div>
+    <div className='chat-window'>
       <div className='chat-header'>
         <p>Live Chat</p>
       </div>
       <div className='chat-body'>
-
+        <ScrollToBottom className='message-container'>
+      {messageList.map((messageContent) => {
+        return ( <div className='message' id={userName === messageContent.author ? "you" : "other"}>
+          <div>
+            <div className='message-content'>
+              <p>{messageContent.message}</p>
+            </div>
+            <div className='message-meta'>
+              <p id="author">{messageContent.author}</p>
+            </div>
+          </div>
+        </div>
+        )
+      })}
+      </ScrollToBottom>
       </div>
       <div className='chat-footer'>
-        <input type="text" onChange={(event) => {setCurrentMessage(event.target.value)}}/>
+        <input type="text" onChange={(event) => {setCurrentMessage(event.target.value)}} onKeyPress={(event) => {event.key === "Enter" && sendMessage()}}/>
         <button onClick={sendMessage}>&#9658;</button>
       </div>
     </div>
