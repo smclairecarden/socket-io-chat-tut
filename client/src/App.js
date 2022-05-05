f1e4e438a10c89fd2e5af4641fe9cff399977b7d
Claire Carden <smclairecarden@gmail.com>
2022-05-05 11:09:36 -0500

    build UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client'
+import { useState } from "react"
+import Chat from './Chat';
+
+const socket = io.connect("http://localhost:3001")
 
 function App() {
+const [userName, setUserName] = useState("")
+const [room, setRoom] = useState("")
+
+const joinRoom = () => {
+  if(userName !== "" && room !== "") {
+    socket.emit("join_room", room)
+    
+  }
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Chat</h1>
+      <h3>Join A Chat</h3>
+      <input type="text" placeholder="Your Name here" onChange={(event) => {setUserName(event.target.value)}}/>
+      <input type="text" placeholder="Room ID..." onChange={(event) => {setRoom(event.target.value)}}/>
+      <button onClick={joinRoom}>Join A Room</button>
+      <Chat socket={socket} username={userName} room={room} />
     </div>
   );
 }
---
+++ client/src/Chat.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react'
+
+
+function Chat({socket, userName, room}) {
+  const [currentMessage, setCurrentMessage] = useState("")
+
+  const sendMessage = async () => {
+    if(currentMessage !== "") {
+      const messageData = {
+        room: room,
+        author: userName,
+        message: currentMessage,
+        time: new Date(Date.now()),
+      }
+      await socket.emit("send_message", messageData)
+
+    }
+  }
+
+
+  useEffect(() => {
+    
+  })
+
+  return (
+    <div>
+      <div className='chat-header'>
+        <p>Live Chat</p>
+      </div>
+      <div className='chat-body'>
+
+      </div>
+      <div className='chat-footer'>
+        <input type="text" onChange={(event) => {setCurrentMessage(event.target.value)}}/>
+        <button onClick={sendMessage}>&#9658;</button>
+      </div>
+    </div>
+  )
+}
+
+export default Chat
